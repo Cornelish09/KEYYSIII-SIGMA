946b98e3e57b2796ba75441a3d42fe4284e8aa9d
Cornelish09 <cornelishyoseptanu09@gmail.com>
2026-02-23 21:44:11 +0700

    mm

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
   server: {
-    port: 3000,
-    strictPort: false
+    port: 5173,
+    strictPort: true
   }
 });
